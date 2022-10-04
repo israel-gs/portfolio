f32682b05bd5cc1a0d16fd4981f198bebf7beb36
José Israel Gutierrez Salazar <israelgs83@gmail.com>
2022-10-03 19:11:14 -0500

    [CHANGE] Añadiendo imagen de perfil

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -10,13 +10,15 @@ export default function ContactInfo() {
   return (
     <section className="font-satoshi bg-dark-800 text-white py-28 px-14 lg:py-40 lg:px-20 flex justify-center gap-16 flex-wrap">
       <div className="w-full lg:w-auto">
-        <h2 className="text-5xl font-bold mb-12">Get in touch</h2>
+        <h2 className="text-4xl md:text-5xl font-bold mb-8 md:mb-12">
+          Get in touch
+        </h2>
         <Button onClick={handleDownload}>Download resume</Button>
       </div>
       <div className="flex flex-col gap-5 w-full lg:w-auto">
         <div className="flex gap-12 flex-wrap">
           <p className="mb-4 flex flex-col flex-1">
-            <span className="font-bold text-3xl">Email</span>
+            <span className="font-bold text-2xl md:text-3xl">Email</span>
             <a
               href="mailto:israelgs83@gmail.com"
               className="text-white hover:text-primary-500 transition-colors text-2xl"
@@ -25,7 +27,7 @@ export default function ContactInfo() {
             </a>
           </p>
           <p className="mb-4 flex flex-col flex-1">
-            <span className="font-bold text-3xl">Phone</span>
+            <span className="font-bold text-2xl md:text-3xl">Phone</span>
             <a
               href="tel:51934090580"
               className="text-white hover:text-primary-500 transition-colors text-2xl"
@@ -36,7 +38,7 @@ export default function ContactInfo() {
         </div>
         <div className="flex gap-12 flex-wrap">
           <p className="mb-4 flex flex-col flex-1">
-            <span className="font-bold text-3xl">Social</span>
+            <span className="font-bold text-2xl md:text-3xl">Social</span>
             <div className="flex">
               <IconButton
                 href="https://www.linkedin.com/in/israel-gs/"
