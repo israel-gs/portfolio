34bcd9a91c117949195a7a98748a8d8d8a73e5f8
José Israel Gutierrez Salazar <israelgs83@gmail.com>
2022-10-06 01:56:31 -0500

    [REFACTOR] Refactorizando todo el protafolio

--- src/helpers/changeBackgroundOnScroll.js
+++ src/helpers/changeBackgroundOnScroll.js
@@ -10,3 +10,20 @@ export function changeBackgroundOnScroll(selector, classes) {
     }
   });
 }
+
+export function changeClassOnScroll(observedSelector, targetSelector, classes) {
+  const targetElement = document.querySelector(targetSelector);
+  const observerElement = document.querySelector(observedSelector);
+  const options = {
+    threshold: 0.2,
+  };
+  const observer = new IntersectionObserver(([entry]) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      targetElement.classList.remove(...classes);
+    } else {
+      targetElement.classList.add(...classes);
+    }
+  }, options);
+  observer.observe(observerElement);
+}
--- src/helpers/scrollToElement.js
+++
@@ -1,6 +0,0 @@
-export function scrollToElement(elementSelector) {
-  const element = document.querySelector(elementSelector);
-  if (element) {
-    element.scrollIntoView({ behavior: "smooth" });
-  }
-}
